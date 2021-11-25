47032d8c78944da3aab20899a2d8be720d0737e3
mayowa86 <mayowasamuel86@gmail.com>
2021-11-25 17:36:43 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+const tweetForm = document.querySelector('#tweetForm');
+
+const duser =  document.querySelector('#duser')
+const dcomment = document.querySelector('#dcomment')
+
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = tweetForm.elements.username.value;
+    const comment = tweetForm.elements.comment.value;
+
+    addcomment(username, comment)
+});
+
+const addcomment = (username, comment) =>{
+    const user = document.createElement('h3')
+    const newComment = document.createElement('p')
+
+    user.append(username);
+    newComment.append(comment);
+
+    duser.append(user);
+    dcomment.append(newComment);  
+    
+    username.value="";
+    comment.value="";
+}
+
+
+
+
+
